d624ace1a8892c67a7aa7b521612596173184aa0
Afor Linda Odoma <aforodoma@gmail.com>
2023-10-11 15:00:37 +0100

    21 restructure root module (#22)
    * #19 WIP
    
    * Create .keep
    
    * Create week 0
    
    * Update week 0
    
    * Update README.md
    
    * Update README.md
    
    * # 19 fix file name
    
    * Update week0.md
    
    * Update week0.md
    
    * Create week1.md
    
    * Create week1.md
    
    * Update README.md
    
    * Update week1.md
    
    * # 19 change structure of root module and add tagging to bucket

--- main.tf
+++ main.tf
@@ -1,32 +1,3 @@
-terraform {
-  cloud {
-    organization = "afor-linda-odoma"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-
-  
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.19.0"
-    }
-  }
-}
-
-provider "aws" {
-}
-
-provider "random" {
-  # Configuration options
-}
-
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   lower = true
@@ -41,8 +12,9 @@ resource "aws_s3_bucket" "example" {
   # Bucket naming rules
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
   bucket = "my-terraform-bucket-88afor"
+ 
+  tags = {
+    UserUuid = var.user_uuid 
+  }
 }
 
-output "random_bucket_name" {
-  value = "my-terraform-bucket-88afor"
-}
